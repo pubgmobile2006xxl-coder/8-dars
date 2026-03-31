8b8e3515feeadb6c6255ea48e7e29c83c48432db
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-03-31 11:24:08 +0500

    boshladin

---
+++ main.js
@@ -0,0 +1,137 @@
+// 1-misol
+
+// let a = +prompt("a son kiriting");
+// if (a > 0) {
+//     a = a + 1;
+// }
+// console.log(a);
+
+// 2-misol
+
+// let a = +prompt("a son kiriting");
+// if (a > 0) {
+//     a = a + 1;
+// } else if (a < 0) {
+//     a = a - 2;
+// } else if (a === 0) {
+//     a = 10
+// }{
+// }
+// console.log(a);
+
+// 3-misol
+
+// let a = prompt("a son kiriting");
+// let b = prompt("b son kiriting");
+// let c = prompt("c son kiriting");
+
+// let count = 0;
+
+// if (a > 0) {
+//     count++;
+// }else{
+
+// }
+// if (b > 0) {
+//     count++;
+// } else {
+    
+// }
+// if (c > 0) {
+//     count++;
+// } else {
+    
+// }
+// console.log("Musbat sonlar soni",count);
+
+// 4-misol
+
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// if (a > b) {
+//     console.log("Katta son",a);
+// } else if (b > a) {
+//     console.log("Katta son",b);
+// } else {
+//     console.log("Ikkala son teng");
+// }
+
+// 5-misol
+
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// if (a < b) {
+//     console.log("Kichik son tartib raqami: a");
+// } else if (b < a) {
+//     console.log("Kichik son tartib raqami: b");
+// } else {
+//     console.log("Ikkala son teng");
+// }
+
+// 6-misol
+
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// if (a > b ) {
+//     console.log("Katta", a);
+//     console.log("Kichkina", b);
+// } else if ( b > a) {
+//     console.log("Katta", b);
+//     console.log("Kichkina", a);
+// } else {
+//     console.log("Ikkala son teng", a);
+// }{  
+// }
+
+// 7-misol
+
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// if (a > b) {
+//     let temp = a;
+//     a = b;
+//     b = temp;
+// } else {
+// }
+// console.log('a =', a);
+// console.log('b =', b);
+
+// 8- misol
+
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// if (a !== b) {
+//     a = a + b;
+//     b = a ;
+// } else {
+//     a = 0;
+//     b = 0;
+// }
+// console.log("a = ", a);
+// console.log("b = ", b);
+
+// 9-misol
+
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// if (a !== b) {
+//     let max = (a > b) ? a : b;
+//     a = max
+//     b = max
+// } else {
+//     a = 0;
+//     b = 0;
+// }
+// console.log("a =", a);
+// console.log("b =", b);
+
+// 10-misol
+// let a = +prompt("a son kriting");
+// let b = +prompt("b son kriting");
+// let c = +prompt("c son kriting");
+// let min = Math. min(a , b, c,);
+// console.log("Eng kichik son", min);
+
+// uy ishi insta
+
+
